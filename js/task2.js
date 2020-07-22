7e33c9b4fc8765fb9de5c3b7a23359297dbd8324
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-07-22 15:02:49 +0300

    added home work 4

---
+++ js/task2.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+const added = inventory.add.bind(inventory);
+
+const remove = inventory.remove.bind(inventory);
+
+invokeInventoryAction('Medkit', added);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
